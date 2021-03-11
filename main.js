500a682865db6997859cc4e771c4cabf070e0f82
Karolína Chovancová <karolina.chloubova@gmail.com>
2021-03-11 13:30:00 +0100

    úkol dokončen

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+
+
+let ctverecek = document.querySelector(".ctverecek");
+let zobrazVysledek = document.querySelector(".zobraz_vysledek");
+
+function pocitej() {
+  ctverecek.style.backgroundColor = "green";
+  document.querySelector("#vysledek").innerHTML = secti(4, 5);
+}
+
+
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -8,6 +18,7 @@ document.querySelector("#vysledek").innerHTML = secti(4, 5);
  * @param {int} a
  * @param {int} b
  */
+
 function secti(a, b) {
   let c = a + b;
   return c;
@@ -18,4 +29,7 @@ function secti(a, b) {
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
-}
+  console.log("Gratulace, právě jsi spustila tuto funkci!")
+  document.querySelector(".ctverecek").innerHTML="Gratulace, právě jsi spustila tuto funkci!";
+  ctverecek.style.lineHeight = "25px"
+  }
